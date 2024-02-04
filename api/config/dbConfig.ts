d05fe429d19c8b73ff76457d6bdd3cd42236cb80
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-04 14:14:04 -0800

    Create dbConfig.ts

---
+++ api/config/dbConfig.ts
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
