bb5fbf8d770bbca311b3f974961feeef3111a09d
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-04 14:15:34 -0800

    Update dbConfig.ts

--- api/config/dbConfig.ts
+++ api/config/dbConfig.ts
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, {
+const dbURI = process.env.MONGODB_URI;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
   useNewUrlParser: true, 
   useUnifiedTopology: true,
   useFindAndModify: false,
