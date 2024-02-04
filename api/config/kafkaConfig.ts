ffbe96582a54cf3832771a6c0679597500d8e375
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-04 14:13:30 -0800

    Create kafkaConfig.ts

---
+++ api/config/kafkaConfig.ts
@@ -0,0 +1,12 @@
+const { Kafka } = require('kafkajs');
+const kafkaClientOptions = {
+  clientId: 'saas_repricer',
+  brokers: JSON.parse(process.env.KAFKA_BROKERS), // Expected as a JSON array
+  ssl: true,
+  sasl: {
+    mechanism: 'plain', // or scram-sha-256, scram-sha-512
+    username: process.env.KAFKA_USERNAME,
+    password: process.env.KAFKA_PASSWORD
+  }
+};
+const kafka = new Kafka(kafkaClientOptions);
