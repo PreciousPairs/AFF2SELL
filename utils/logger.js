09913a4b1187683f8957923e53e3a5dbe1ae520a
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-04 23:43:45 -0800

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,13 @@
+// ./utils/logger.js
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(format.timestamp(), format.json()),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+module.exports = logger;
