c35289e98987444ed2989f92f2c106d1624a9c1c
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-04 21:29:04 -0800

    Create authorize.js

---
+++ middleware/authorize.js
@@ -0,0 +1,9 @@
+// middleware/authorize.js
+exports.authorize = (roles = []) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Unauthorized' });
+    }
+    next();
+  };
+};
