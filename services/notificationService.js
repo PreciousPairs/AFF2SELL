2233b4a21e6991f3e1b4bac45fc0229a5d65ee03
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-04 22:01:43 -0800

    Update notificationService.js

--- services/notificationService.js
+++ services/notificationService.js
@@ -1,16 +1,30 @@
-const { sendEmail, sendSMS, pushNotification } = require('../utils/notificationMethods');
+const { sendEmail, sendSMS, pushNotification } = require('../utils/notificationUtils');
+const User = require('../models/User');
+const { getTemplateByKey } = require('./templateService');
 
-exports.notifyUsers = async (notificationType, message) => {
-  const users = await getUsersToNotify(notificationType); // Assume this function fetches users based on notification preferences
-  users.forEach(user => {
-    if (user.notificationPreferences.email) {
-      sendEmail(user.email, message);
-    }
-    if (user.notificationPreferences.sms) {
-      sendSMS(user.phone, message);
-    }
-    if (user.notificationPreferences.pushNotification) {
-      pushNotification(user.deviceId, message);
-    }
-  });
+exports.sendNotification = async (templateKey, userCriteria = {}) => {
+    const template = await getTemplateByKey(templateKey);
+    const users = await User.find(userCriteria);
+
+    users.forEach(user => {
+        const { preferences } = user;
+        template.channels.forEach(channel => {
+            if (preferences.notifications[channel]) {
+                const message = template.message; // In practice, you might want to personalize this message.
+                switch (channel) {
+                    case 'email':
+                        sendEmail(user.email, template.title, message);
+                        break;
+                    case 'sms':
+                        sendSMS(user.phone, message);
+                        break;
+                    case 'push':
+                        pushNotification(user.pushToken, message);
+                        break;
+                    default:
+                        console.log(`Unsupported notification channel: ${channel}`);
+                }
+            }
+        });
+    });
 };
