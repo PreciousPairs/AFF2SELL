eef23abf5bb28912f4155de2059dd92c5ea3b62e
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-04 23:44:22 -0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,15 @@
 // app.js
 const { consumeMessages } = require('./kafka/consumer');
 const { processMessage } = require('./services/messageProcessor'); // Define your message processing logic here
+// app.js or server.js
+const express = require('express');
+const app = express();
+const { securityMiddlewares } = require('./middleware/securityMiddleware');
+
+// Apply security middlewares
+securityMiddlewares(app);
+
+// Continue with the rest of your application setup...
 
 const topic = 'your-topic-name';
 
