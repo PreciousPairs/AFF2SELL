39b21553fe642648eefa7d5e087fc1a484230dc0
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-04 23:34:42 -0800

    Create validation.js

---
+++ middleware/validation.js
@@ -0,0 +1,7 @@
+exports.validateRequestBody = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).json({ message: error.details[0].message });
+    next();
+  };
+};
