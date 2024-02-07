e5e7009bf939155f1ebc4f1691bb15a526df9df8
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-06 20:25:54 -0800

    Update kafkaConfig.ts

--- api/config/kafkaConfig.ts
+++ api/config/kafkaConfig.ts
@@ -1,15 +1,29 @@
-// Kafka configuration
-const { Kafka } = require('kafkajs');
+KafkaConfig.ts
+import { Kafka, logLevel } from 'kafkajs';
 
-const kafkaClientOptions = {
-  clientId: 'saas_repricer',
-  brokers: JSON.parse(process.env.KAFKA_BROKERS), // Expected as a JSON array
-  ssl: true,
-  sasl: {
-    mechanism: 'plain', // or scram-sha-256, scram-sha-512
-    username: process.env.KAFKA_USERNAME,
-    password: process.env.KAFKA_PASSWORD
-  }
-};
+class KafkaConfig {
+    private static kafkaInstance: Kafka;
+    
+    private constructor() {}
 
-const kafka = new Kafka(kafkaClientOptions);
+    public static getInstance(): Kafka {
+        if (!this.kafkaInstance) {
+            this.kafkaInstance = new Kafka({
+                clientId: 'saas-repricer',
+                brokers: process.env.KAFKA_BROKERS?.split(',') || ['localhost:9092'],
+                ssl: true,
+                sasl: {
+                    mechanism: 'plain', // Or 'scram-sha-256'/'scram-sha-512'
+                    username: process.env.KAFKA_USERNAME,
+                    password: process.env.KAFKA_PASSWORD,
+                },
+                logLevel: logLevel.ERROR, // Adjust log level based on environment
+            });
+            console.log('Kafka client configured successfully.');
+        }
+        return this.kafkaInstance;
+    }
+}
+
+export default KafkaConfig;
+
