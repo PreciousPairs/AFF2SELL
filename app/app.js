f9fb1f44c256811a706714f616d37bab6c00fc54
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-06 18:51:53 -0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,5 +1,3 @@
-// app.js
-
 require('dotenv').config(); // Load environment variables from .env file at the beginning
 const express = require('express');
 const helmet = require('helmet'); // Security middleware for setting various HTTP headers
