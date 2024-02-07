3ebb5df2ac67e69075d4f0db99e2814d1413a5b3
PreciousPairs <143674101+PreciousPairs@users.noreply.github.com>
2024-02-06 21:57:17 -0800

    Update utils

---
+++ utils/validator.ts
@@ -0,0 +1,4 @@
+export const validateEmail = (email: string): boolean => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()\[\]\\.,;:\s@"]+\.)+[^<>()\[\]\\.,;:\s@"]{2,})$/i;
+  return re.test(email.toLowerCase());
+};
